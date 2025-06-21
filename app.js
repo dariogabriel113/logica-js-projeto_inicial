63638cb056359622c79e37e4cdb921195788508d
Dario Gabriel <dariogabriel113@gmail.com>
2025-06-21 16:44:36 -0300

    Aula 3 - Loops e tentativas - Cria loop que funciona ate encontrar numero secreto

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 alert('Boas vindas ao jogo do Número Secreto!');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
-//se chute for igual ao número secreto
-if (numeroSecreto == chute) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(');
+//enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    //se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas}`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+
+        tentativas++;
+    }
 }
